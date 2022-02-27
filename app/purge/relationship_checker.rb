bc3457b10074f4b9a660cbb8e01d738a34fac794
shalvah <diakon.ng@gmail.com>
2022-02-27 21:07:04 +0100

    Extract search handler to separate project

--- app/purge/relationship_checker.rb
+++ app/purge/relationship_checker.rb
@@ -6,13 +6,12 @@ module Purge
 
   class RelationshipChecker
     def self.build(user, following)
-      new(user, following, Services[:lambda_client])
+      new(user, following)
     end
 
-    def initialize(user, following, lambda_client, days_ago = 90)
+    def initialize(user, following, days_ago = 90)
       @user = AppUser.from(user)
       @following = following
-      @lambda_client = lambda_client
       @days_ago = days_ago
     end
 
@@ -36,28 +35,24 @@ module Purge
 
     def query_tweets_exist?(query)
       payload = {
-        query: query,
+        queries: [query],
         checkExistenceOnly: true
       }
-      response = @lambda_client.invoke({
-        function_name: "volgnie-dev-search_twitter",
-        invocation_type: "RequestResponse",
-        payload: { body: payload }.to_json,
-      })
 
-      # Docs say it's a String, but it seems to be returning a StringIO
-      result = JSON.parse(response.payload.is_a?(StringIO) ? response.payload.string : response.payload)
-      raise_if_search_failed(result, query)
+      Honeybadger.context({ searchQuery: payload.queries })
+
+      req = RestClient::Request.new(
+        method: "post",
+        url: ENV["ZEARCH_ENDPOINT"],
+        headers: { authorization: ENV["ZEARCH_KEY"], content_type: :json },
+        body: payload,
+        timeout: 30
+      )
+      response = req.execute
+
+      result = JSON.parse(response.body)
+
       result["exists"]
-    end
-
-    def raise_if_search_failed(result, query)
-      if result["errorMessage"]
-        Honeybadger.context({ query: query })
-        raise SearchHandlerFailed.new(
-          "Search handler returned an error: #{result["errorType"]} - #{result["errorMessage"]}.\nTrace: #{result["trace"]}"
-        )
-      end
     end
   end
 end
