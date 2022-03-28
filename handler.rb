892bc54675f6bb0c57b5ce5f7bdd1d940fa7248e
shalvah <diakon.ng@gmail.com>
2022-03-28 09:58:35 +0100

    Add count option for retrues

--- handler.rb
+++ handler.rb
@@ -82,7 +82,8 @@ end
 
 def retry(event:, context:)
   full_name = "volgnie-dev-#{event["function"]}"
-  events = Services[:cache].lrange("purge-dlq-#{full_name}", 0, -1)
+  count = event["count"] || 0
+  events = Services[:cache].lrange("purge-dlq-#{full_name}", 0, count - 1)
   raise "Couldn't find any event data" if events.nil? || events.empty?
   Services[:cache].del("purge-dlq-#{full_name}") # Not atomic, but we'll worry about that later
 
